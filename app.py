3e49693fd75b614c0fbe9d4cfbb3100c11bb2d0a
Enjin26 <106729356+Enjin26@users.noreply.github.com>
2022-06-02 12:58:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ Created on Thu Jun  2 12:28:55 2022
 
 import streamlit as st
 st.write("hello")
+
+data_file = st.file_uploader("Upload CSV",type=["csv"])
+if data_file is not None:
+    file_details = {"filename":data_file.name, "filetype":data_file.type,"filesize":data_file.size}
+    st.write(file_details)
